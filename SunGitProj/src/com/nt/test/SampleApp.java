89f8c85efedd09271991d684c6ad4d10e0de0cd5
lenovo <lenovo@192.168.0.16>
2018-06-10 22:59:18 +0530

    dev1-changes program1

--- SunGitProj/src/com/nt/test/SampleApp.java
+++ SunGitProj/src/com/nt/test/SampleApp.java
@@ -5,8 +5,12 @@ public class SampleApp {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 
+		for (int i = 1; i <= 10; ++i) {
+			System.out.println(i);
+		}
+
 	}
 
 }
-//hi
-//Hello
+// hi
+// Hello
